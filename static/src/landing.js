bcda9e7ac43cd4448797cf3e2a8caa19496c08be
Jason Priem <jp@jasonpriem.org>
2017-03-22 22:14:22 -0700

    new landing page.

--- static/src/landing.js
+++ static/src/landing.js
@@ -20,86 +20,34 @@ angular.module('landing', [
     .controller("LandingPageCtrl", function ($scope,
                                              $http,
                                              $rootScope,
+                                             $interval,
                                              $timeout) {
 
-        console.log("i am the landing page ctrl")
-        $scope.main = {}
-
-
-        var animate = function(step){
-            $scope.animation = step + "start"
-            console.log("set animation", $scope.animation)
-            $timeout(function(){
-                $scope.animation = step + "finish"
-                console.log("set animation", $scope.animation)
-            }, 350)
+        console.log("landing page.")
+        function numberWithCommas(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         }
 
-
-        var baseUrl = "https://api.oadoi.org/v1/publication/doi/"
-        $scope.exampleDoi = "10.1016/j.tree.2007.03.007"
-        $scope.exampleDoi = "10.1038/nature12373"
-
-        $scope.selectExample = function(){
-            $scope.main.exampleSelected = true
-            $scope.main.doi = $scope.exampleDoi
-        }
-        $scope.tryAgain = function(){
-            $scope.animation = null
-            $scope.main = {}
-        }
-
-        
+        var oneServedEvery = 2000;  // one served every 2000 ms
+        var startTimestampMs = 1485907200 * 1000;  // feb 1 2017
+        var elapsedMs = Date.now() - startTimestampMs;
+        var numServed = Math.floor(elapsedMs / oneServedEvery);
 
 
-        $scope.$watch(function(s){return s.main.doi }, function(newVal, oldVal){
-            console.log("doi change", newVal, oldVal)
-            if (!newVal){
-                return false
-            }
-
-            if (newVal.indexOf("10/") == 0 || newVal.indexOf("doi.org/10/") >= 0){
-                $scope.main = {}
-                "Sorry, we don't support ShortDOI yet."
-                $rootScope.showAlert(
-                    ga("send", "event", "input DOI", "shortDOI"  )
-                )
-                return true
-            }
+        $scope.d = {}
+        $scope.d.numServed = numberWithCommas(numServed)
 
 
-            if (newVal.indexOf("10.") >= 0) {
-                animate(1)
-                $http.get(baseUrl + newVal)
-                    .success(function(resp){
-                        console.log("got response back", resp.results[0])
-                        if (newVal.indexOf($scope.exampleDoi) >= 0){
-                            console.log("this is the sample DOI...waiting to return result.")
-                            ga("send", "event", "input DOI", "sample DOI"  )
-
-                            $timeout(function(){
-                                console.log("returning the result now")
-                                animate(2)
-                                $scope.main.resp = resp.results[0]
-                            }, 1000)
-                        }
-                        else {
-                            animate(2)
-                            ga("send", "event", "input DOI", "user-supplied DOI" )
-                            $scope.main.resp = resp.results[0]
-                        }
+        $interval(function(){
+            numServed += 1
+            $scope.d.numServed = numberWithCommas(numServed)
+            console.log("tick")
+        }, oneServedEvery)
 
 
-                    })
-            }
-            else {
-                $scope.main = {}
-                ga("send", "event", "input DOI", "typed the DOI"  )
-                $rootScope.showAlert(
-                    "Sorry, you have to paste DOIs here...you can't type them."
-                )
-            }
-        })
+
+
+
 
     })
 
--- static/ti.js
+++ static/ti.js
@@ -176,86 +176,34 @@ angular.module('landing', [
     .controller("LandingPageCtrl", function ($scope,
                                              $http,
                                              $rootScope,
+                                             $interval,
                                              $timeout) {
 
-        console.log("i am the landing page ctrl")
-        $scope.main = {}
-
-
-        var animate = function(step){
-            $scope.animation = step + "start"
-            console.log("set animation", $scope.animation)
-            $timeout(function(){
-                $scope.animation = step + "finish"
-                console.log("set animation", $scope.animation)
-            }, 350)
+        console.log("landing page.")
+        function numberWithCommas(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         }
 
-
-        var baseUrl = "https://api.oadoi.org/v1/publication/doi/"
-        $scope.exampleDoi = "10.1016/j.tree.2007.03.007"
-        $scope.exampleDoi = "10.1038/nature12373"
-
-        $scope.selectExample = function(){
-            $scope.main.exampleSelected = true
-            $scope.main.doi = $scope.exampleDoi
-        }
-        $scope.tryAgain = function(){
-            $scope.animation = null
-            $scope.main = {}
-        }
-
-        
+        var oneServedEvery = 2000;  // one served every 2000 ms
+        var startTimestampMs = 1485907200 * 1000;  // feb 1 2017
+        var elapsedMs = Date.now() - startTimestampMs;
+        var numServed = Math.floor(elapsedMs / oneServedEvery);
 
 
-        $scope.$watch(function(s){return s.main.doi }, function(newVal, oldVal){
-            console.log("doi change", newVal, oldVal)
-            if (!newVal){
-                return false
-            }
-
-            if (newVal.indexOf("10/") == 0 || newVal.indexOf("doi.org/10/") >= 0){
-                $scope.main = {}
-                "Sorry, we don't support ShortDOI yet."
-                $rootScope.showAlert(
-                    ga("send", "event", "input DOI", "shortDOI"  )
-                )
-                return true
-            }
+        $scope.d = {}
+        $scope.d.numServed = numberWithCommas(numServed)
 
 
-            if (newVal.indexOf("10.") >= 0) {
-                animate(1)
-                $http.get(baseUrl + newVal)
-                    .success(function(resp){
-                        console.log("got response back", resp.results[0])
-                        if (newVal.indexOf($scope.exampleDoi) >= 0){
-                            console.log("this is the sample DOI...waiting to return result.")
-                            ga("send", "event", "input DOI", "sample DOI"  )
-
-                            $timeout(function(){
-                                console.log("returning the result now")
-                                animate(2)
-                                $scope.main.resp = resp.results[0]
-                            }, 1000)
-                        }
-                        else {
-                            animate(2)
-                            ga("send", "event", "input DOI", "user-supplied DOI" )
-                            $scope.main.resp = resp.results[0]
-                        }
+        $interval(function(){
+            numServed += 1
+            $scope.d.numServed = numberWithCommas(numServed)
+            console.log("tick")
+        }, oneServedEvery)
 
 
-                    })
-            }
-            else {
-                $scope.main = {}
-                ga("send", "event", "input DOI", "typed the DOI"  )
-                $rootScope.showAlert(
-                    "Sorry, you have to paste DOIs here...you can't type them."
-                )
-            }
-        })
+
+
+
 
     })
 
@@ -659,179 +607,71 @@ angular.module("browser-tools.tpl.html", []).run(["$templateCache", function($te
 
 angular.module("landing.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("landing.tpl.html",
-    "<div class=\"top-screen\" layout=\"row\" layout-align=\"center center\">\n" +
-    "    <div class=\"content\">\n" +
-    "\n" +
-    "        <div class=\"enter-doi no-doi demo-step\"\n" +
-    "             ng-class=\"{'animated fadeOutDown': animation=='2start'}\"\n" +
-    "             ng-hide=\"animation=='2start' || animation=='2finish'\">\n" +
-    "\n" +
-    "            <h1 class=\"animation-{{ !!animation }} site-heading\">\n" +
-    "                <img src=\"static/img/oadoi-logo.png\" alt=\"\" class=\"logo\">\n" +
-    "                Leap over tall paywalls in a single bound.\n" +
-    "\n" +
-    "                <!--\n" +
-    "                Link to the open version of any DOI\n" +
-    "\n" +
-    "                -->\n" +
-    "\n" +
-    "\n" +
-    "                <!--\n" +
-    "                Use oadoi.org/your_doi\n" +
-    "                to find the Open Access version\n" +
-    "                -->\n" +
-    "            </h1>\n" +
-    "\n" +
-    "            <div class=\"under\">\n" +
-    "                <div class=\"input-row\">\n" +
-    "                    <md-input-container md-no-float class=\"md-block example-selected-{{ main.exampleSelected }}\" flex-gt-sm=\"\">\n" +
-    "                        <!--\n" +
-    "                        <label ng-show=\"!animation\" class=\"animating-{{ animation }}\" >Paste your DOI here</label>\n" +
-    "                        -->\n" +
-    "\n" +
-    "                        <div class=\"us\"  >oadoi.org/</div>\n" +
-    "                        <input ng-model=\"main.doi\"\n" +
-    "                               ng-disabled=\"animation\">\n" +
-    "                        <md-progress-circular md-diameter=\"26px\"></md-progress-circular>\n" +
-    "\n" +
-    "                    </md-input-container>\n" +
-    "\n" +
+    "<div class=\"page landing\">\n" +
+    "    <div class=\"top-screen\" layout=\"row\" layout-align=\"center center\">\n" +
+    "        <div class=\"content\">\n" +
+    "            <div class=\"main-banner\">\n" +
+    "                <div class=\"tagline\">\n" +
+    "                    <span class=\"number\">{{ d.numServed }}</span>\n" +
+    "                    <span class=\"text\">fulltext articles served.</span>\n" +
     "                </div>\n" +
-    "                <div class=\"text\">\n" +
-    "                    <div class=\"example-doi\"\n" +
-    "                         ng-class=\"{'animated fadeOut': animation}\"\n" +
-    "                         ng-hide=\"animation\">\n" +
-    "                        <span class=\"label\">Paste in a DOI, or try this example: </span>\n" +
-    "                        <span class=\"val\" ng-click=\"selectExample()\">http://doi.org/{{ exampleDoi }}</span>\n" +
-    "                        <a href=\"https://doi.org/{{ exampleDoi }}\" target=\"_blank\">[paywall]</a>\n" +
+    "                <div class=\"subtagline\">\n" +
+    "                    Search our database of over 100 million scholarly resources\n" +
+    "                    and find free fulltext from open-access repositories worldwide.\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"products\">\n" +
+    "                <div class=\"product unpaywall\">\n" +
+    "                    <div class=\"text\">\n" +
+    "                        <h2>\n" +
+    "                            <i class=\"fa fa-user\"></i>\n" +
+    "                            Everyone\n" +
+    "                        </h2>\n" +
+    "                        <div class=\"about\">\n" +
+    "                            The Unpaywall browser extension\n" +
+    "                            bypasses paywalls as you browse. Installed by\n" +
+    "                            500 new users every day.\n" +
+    "                        </div>\n" +
     "                    </div>\n" +
-    "\n" +
+    "                    <div class=\"cta\">Install it now</div>\n" +
     "                </div>\n" +
-    "            </div>\n" +
-    "        </div>\n" +
+    "                <div class=\"product sfx\">\n" +
+    "                    <div class=\"text\">\n" +
+    "                        <h2>\n" +
+    "                            <i class=\"fa fa-university\"></i>\n" +
+    "                            Libraries\n" +
+    "                        </h2>\n" +
+    "                        <div class=\"about\">\n" +
+    "                            Our SFX integration adds millions of open-access articles\n" +
+    "                            to your library's holdings. Used by 600 libraries worldwide.\n" +
+    "                        </div>\n" +
+    "                    </div>\n" +
+    "                    <div class=\"cta\">Learn more</div>\n" +
+    "                </div>\n" +
+    "                <div class=\"product api\">\n" +
+    "                    <div class=\"text\">\n" +
+    "                        <h2>\n" +
+    "                            <i class=\"fa fa-cogs\"></i>\n" +
+    "                            Developers\n" +
+    "                        </h2>\n" +
+    "                        <div class=\"about\">\n" +
+    "                            Build your own apps or do research using\n" +
+    "                            our free, open, and scalable REST API.\n" +
+    "                        </div>\n" +
+    "                    </div>\n" +
+    "                    <div class=\"cta\">Read the docs</div>\n" +
+    "                </div>\n" +
     "\n" +
     "\n" +
-    "\n" +
-    "\n" +
-    "        <div class=\"has-results demo-step\"\n" +
-    "             ng-class=\"{'animated fadeInDown': animation==='2finish'}\"\n" +
-    "             ng-show=\"animation && animation==='2finish'\">\n" +
-    "\n" +
-    "\n" +
-    "            <h1 ng-show=\"main.resp.is_free_to_read\"><i class=\"fa fa-check\"></i> Success!</h1>\n" +
-    "            <h1 ng-show=\"!main.resp.is_free_to_read\"><i class=\"fa fa-times\"></i> No dice</h1>\n" +
-    "\n" +
-    "            <div class=\"result-url\">\n" +
-    "                <span class=\"label\">Direct link: </span>\n" +
-    "                <a class=\"main\" href=\"https://oadoi.org/{{ main.resp.doi }}\" target=\"_blank\">\n" +
-    "                    <i class=\"fa fa-unlock\" ng-show=\"main.resp.free_fulltext_url\"></i>\n" +
-    "                    <i class=\"fa fa-lock\" ng-show=\"!main.resp.free_fulltext_url\"></i>\n" +
-    "                    <span class=\"us\">oadoi.org/</span><span class=\"doi\">{{ main.resp.doi }}</span>\n" +
-    "                    <i class=\"fa fa-external-link\"></i>\n" +
-    "                </a>\n" +
-    "\n" +
-    "                <a class=\"mobile-help\" href=\"https://oadoi.org/{{ main.resp.doi }}\" target=\"_blank\"><i class=\"fa fa-external-link\"></i> click to view</a>\n" +
-    "\n" +
-    "                <a class=\"api\" href=\"https://api.oadoi.org/v1/publication/doi/{{ main.resp.doi }}\" class=\"api-url\" target=\"_blank\">\n" +
-    "                    <i class=\"fa fa-cogs\"></i>\n" +
-    "                    View this result in the API\n" +
-    "                </a>\n" +
-    "\n" +
-    "            </div>\n" +
-    "            <div class=\"result-explanation\">\n" +
-    "                <span class=\"hybrid success result\"\n" +
-    "                      ng-show=\"main.resp.is_subscription_journal && main.resp.oa_color=='gold'\">\n" +
-    "                    This article is openly available as Hybrid OA in a subscription journal,\n" +
-    "                </span>\n" +
-    "\n" +
-    "                <span class=\"gold journal success result\"\n" +
-    "                      ng-show=\"main.resp.oa_color=='gold' && !main.resp.is_subscription_journal && main.resp.doi_resolver == 'crossref'\">\n" +
-    "                    This article is openly available in a <span class=\"gold-oa\">Gold OA</span> journal,\n" +
-    "                </span>\n" +
-    "\n" +
-    "                <span class=\"gold repo success result\"\n" +
-    "                      ng-show=\"main.resp.oa_color=='gold' && main.resp.doi_resolver == 'datacite'\">\n" +
-    "                    This article is openly available in a <span class=\"gold-oa\">Gold OA</span> repository,\n" +
-    "                </span>\n" +
-    "\n" +
-    "\n" +
-    "                <span class=\"green success result\"\n" +
-    "                      ng-show=\"main.resp.oa_color=='green'\">\n" +
-    "                    This article was\n" +
-    "                    <a href=\"{{ main.resp.url }}\">published behind a paywall,</a>\n" +
-    "                    but we found a Green OA copy that’s\n" +
-    "                    free to read<span ng-show=\"main.resp.is_boai_license\" class=\"full-oa\"> and reuse</span>,\n" +
-    "                </span>\n" +
-    "\n" +
-    "\n" +
-    "                <span class=\"not-oa failure result\" ng-show=\"!main.resp.free_fulltext_url\">\n" +
-    "                    Sorry, this article is behind a paywall, and we couldn’t find a free copy anywhere.\n" +
-    "                    Unfortunately, this is still true\n" +
-    "                    <a href=\"https://arxiv.org/abs/1206.3664\">for around 80% of scholarly articles.</a>\n" +
-    "                </span>\n" +
-    "\n" +
-    "                <span class=\"license-info\" ng-show=\"main.resp.is_free_to_read\">\n" +
-    "                    <span class=\"license not-specified\" ng-show=\"!main.resp.license\">\n" +
-    "                        with no license specified\n" +
-    "                    </span>\n" +
-    "                    <span class=\"license partly-open\" ng-show=\"main.resp.license && !main.resp.is_boai_license\">\n" +
-    "                        under a\n" +
-    "                        <a href=\"http://sparcopen.org/our-work/howopenisit/\"><span>{{ main.resp.license }}</span> license</a>\n" +
-    "                    </span>\n" +
-    "                    <span class=\"license fully-open\" ng-show=\"main.resp.license && main.resp.is_boai_license\">\n" +
-    "                        under a\n" +
-    "                        <a href=\"http://sparcopen.org/our-work/howopenisit/\">fully open license <span>({{ main.resp.license }})</span></a>\n" +
-    "                    </span>\n" +
-    "                </span>\n" +
-    "\n" +
-    "                <!--\n" +
-    "                <span class=\"host\">\n" +
-    "                    <span class=\"researchgate\" ng-show=\"main.resp.free_fulltext_url.indexOf('har') > 0\">in <a\n" +
-    "                            href=\"http://osc.universityofcalifornia.edu/2015/12/a-social-networking-site-is-not-an-open-access-repository/\">ResearchGate</a>\n" +
-    "                    </span>\n" +
-    "                    <span class=\"figshare\" ng-show=\"main.resp.free_fulltext_url.indexOf('figshare') > 0\">in figshare</span>\n" +
-    "                </span>\n" +
-    "                -->\n" +
-    "\n" +
-    "                <span class=\"period\">.</span>\n" +
     "\n" +
     "            </div>\n" +
     "\n" +
-    "\n" +
-    "\n" +
-    "\n" +
-    "\n" +
-    "\n" +
-    "            <div class=\"results-options\">\n" +
-    "                <!--\n" +
-    "                <a class=\"primary\" href=\"about\"><i class=\"fa fa-info-circle\"></i> learn more</a>\n" +
-    "                -->\n" +
-    "                <a class=\"secondary\"  href=\"\" ng-click=\"tryAgain()\"><i class=\"fa fa-undo\"></i> try another</a>\n" +
-    "                <a class=\"secondary\"  href=\"https://goo.gl/forms/kFZUUZUeM9ze9uXr2\" target=\"_blank\">\n" +
-    "                    <i class=\"fa fa-bullhorn\"></i>\n" +
-    "                    report error\n" +
-    "                </a>\n" +
-    "\n" +
-    "\n" +
-    "                <!--\n" +
-    "                <a href=\"https://twitter.com/intent/tweet?url=https://oadoi.org/&text=Check out @oaDOI_org, an alternative DOI resolver that gets %23openaccess versions of paywalled articles. %23oaWeek2016\"\n" +
-    "                   target=\"_blank\"\n" +
-    "                   class=\"share twitter\">\n" +
-    "                    <i class=\"fa fa-twitter\"></i>\n" +
-    "                    <span class=\"text\">share</span>\n" +
-    "                </a>\n" +
-    "                -->\n" +
-    "            </div>\n" +
     "\n" +
     "        </div>\n" +
     "    </div>\n" +
-    "    <!--\n" +
-    "    <div class=\"more\" ng-show=\"!animation || animation=='2finish'\">\n" +
-    "        <i class=\"fa fa-chevron-down\"></i>\n" +
-    "        Learn more\n" +
-    "    </div>\n" +
-    "    -->\n" +
-    "</div>");
+    "\n" +
+    "</div>\n" +
+    "");
 }]);
 
 angular.module("team.tpl.html", []).run(["$templateCache", function($templateCache) {
